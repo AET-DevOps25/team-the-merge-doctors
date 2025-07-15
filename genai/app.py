be4f96f2a861915cdab1b3392a0adc2f32912b19
Justus Wendroth <justus.wendroth@outlook.de>
2025-07-15 11:22:53 +0200

    changed app.py

--- genai/app.py
+++ genai/app.py
@@ -1,26 +1,3 @@
-# from flask import Flask
-# from config import Config
-# from database import db
-
-# # Import the blueprint object
-# from controllers.api_controller import api
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-
-# # Init extensions
-# db.init_app(app)
-
-# # Register your API blueprint
-# app.register_blueprint(api)
-
-# # Initialize DB tables
-# with app.app_context():
-#     db.create_all()
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5001, debug=False)
-
 from flask import Flask
 from flask_restx import Api
 from config import Config
