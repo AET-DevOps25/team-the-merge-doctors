0fa91c460c78751f589744fc3c90a8ed2de85883
alyassem <ge85zef@mytum.de>
2025-07-19 21:27:14 +0200

    refactor/renames and simple fixes

--- infra/main.tf
+++ infra/main.tf
@@ -8,8 +8,8 @@ provider "aws" {
 
 
 resource "aws_security_group" "ssh" {
-  name        = "allow-ssh"
-  description = "Allow SSH inbound traffic"
+  name        = "allow-ssh-and-web"
+  description = "Allow SSH and Web inbound traffic"
   ingress {
     from_port   = 22
     to_port     = 22
@@ -19,6 +19,12 @@ resource "aws_security_group" "ssh" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
