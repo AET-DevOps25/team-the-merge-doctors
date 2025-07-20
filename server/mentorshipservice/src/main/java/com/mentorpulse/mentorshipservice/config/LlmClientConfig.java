cde83db0dbbe8c79cb48996d5581d2fa2b92bc7d
Ahmed Diab <a7mediab977@gmail.com>
2025-07-20 17:16:35 +0200

    Add a flag for https or http

--- server/mentorshipservice/src/main/java/com/mentorpulse/mentorshipservice/config/LlmClientConfig.java
+++ server/mentorshipservice/src/main/java/com/mentorpulse/mentorshipservice/config/LlmClientConfig.java
@@ -1,5 +1,6 @@
 package com.mentorpulse.mentorshipservice.config;
 
+import org.gradle.internal.impldep.com.google.api.client.util.Value;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class LlmClientConfig {
 
+    @Value("${genai.http-secured:false}")
+    private boolean genaiHttpSecured;
+
     @Value("${genai.host}")
     private String host;
 
@@ -17,7 +21,7 @@ public class LlmClientConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        String url = String.format("http://%s:%s/api/genai", host, port);
+        String url = String.format("%s://%s:%s/api/genai",genaiHttpSecured ? "https": "http", host, port);
         System.out.println(url);
         return builder
                 .rootUri(url)
