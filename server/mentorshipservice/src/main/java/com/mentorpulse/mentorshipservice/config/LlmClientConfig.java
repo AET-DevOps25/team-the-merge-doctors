8a2ddaed48d7d7b69bbeb9c7ae9a1fe9c5788b04
Ahmed Diab <a7mediab977@gmail.com>
2025-07-20 18:07:57 +0200

    Another attempt

--- server/mentorshipservice/src/main/java/com/mentorpulse/mentorshipservice/config/LlmClientConfig.java
+++ server/mentorshipservice/src/main/java/com/mentorpulse/mentorshipservice/config/LlmClientConfig.java
@@ -20,7 +20,12 @@ public class LlmClientConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        String url = String.format("%s://%s:%s/api/genai",genaiHttpSecured ? "https": "http", host, port);
+        String url;
+        if (genaiHttpSecured) {
+            url = String.format("https://%s/api/genai", host);
+        } else {
+            url = String.format("http://%s:%s/api/genai", host, port);
+        }
         System.out.println(url);
         return builder
                 .rootUri(url)
